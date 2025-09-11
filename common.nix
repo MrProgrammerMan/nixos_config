c6788a057ed55b0d5e728edc54ec32125ac4cbab
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-09-11 17:42:34 +0200

    Modularize configuration.nix

---
+++ common.nix
@@ -0,0 +1,80 @@
+{ config, pkgs, ... }:
+
+{
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Oslo";
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nb_NO.UTF-8";
+    LC_IDENTIFICATION = "nb_NO.UTF-8";
+    LC_MEASUREMENT = "nb_NO.UTF-8";
+    LC_MONETARY = "nb_NO.UTF-8";
+    LC_NAME = "nb_NO.UTF-8";
+    LC_NUMERIC = "nb_NO.UTF-8";
+    LC_PAPER = "nb_NO.UTF-8";
+    LC_TELEPHONE = "nb_NO.UTF-8";
+    LC_TIME = "nb_NO.UTF-8";
+  };
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb = {
+    layout = "no";
+    variant = "";
+  };
+
+  console.keyMap = "no";
+
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  users.users.cepheus = {
+    isNormalUser = true;
+    description = "Jonas Hazeland Baugerud";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      zettlr
+    ];
+  };
+
+  environment.shellAliases = {
+    nixdev = "nix develop -i -c bash --norc";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    spotify
+    signal-desktop
+    git
+    vscode
+    discord
+    thunderbird
+    brave
+    wget
+    gimp
+    libreoffice
+    inkscape
+    bitwarden-desktop
+    steam
+    steam-run
+  ];
+  
+  programs.steam.enable = true;
+}
---
+++ hardware-specific.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  # Bootloader configuration
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Hostname
+  networking.hostName = "nixos";
+
+  # Enable NVIDIA drivers
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    powerManagement.enable = false;
+    open = false;
+    prime.sync.enable = false;
+  };
+}
