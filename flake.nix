dc198764de1bafb54b29d9875747744f3d7fe575
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-09-11 17:13:09 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "System configuration flake";
+  
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-25.05"; };
+  };
+  
+  outputs = { self, nixpkgs, ... }: 
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
