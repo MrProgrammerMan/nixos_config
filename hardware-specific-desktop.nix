c6a01a4d8b32229f0f0bd047e80816eafe11b649
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-09-14 14:33:16 +0200

    Flake content update(nixos-desktop)

--- hardware-specific-desktop.nix
+++ hardware-specific-desktop.nix
@@ -10,11 +10,6 @@
 
   system.stateVersion = "25.05";    
 
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
-  };
-
   # Enable NVIDIA drivers
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
