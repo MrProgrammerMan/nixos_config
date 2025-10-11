01489af055c755fa8c530bbfc458766dcaccf06c
= <==>
2025-10-11 15:59:12 +0200

    Flake content update(nixos-laptop)

--- hardware-specific-laptop.nix
+++ hardware-specific-laptop.nix
@@ -15,6 +15,7 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [ intel-media-driver intel-ocl intel-vaapi-driver ];
   };
 
   # If Intel/AMD GPU (most laptops):
