37e2fb4d3d2718a3de6f48bdb32ca236db8430d0
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-10-19 14:57:39 +0000

    Flake content update(desktop)

--- flake.nix
+++ flake.nix
@@ -2,72 +2,32 @@
   description = "System configuration for desktop & laptop";
   
   inputs = {
-    nixpkgs-stable = { url = "github:NixOS/nixpkgs/nixos-25.05"; };
-    nixpkgs-unstable = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
     nvf.url = "github:notashelf/nvf";
   };
   
-  outputs = inputs@{ self, nixpkgs-unstable, nixpkgs-stable, nvf, ... }:
+  outputs = inputs@{ self, nixpkgs, nvf, ... }:
   let
     system = "x86_64-linux";
-    
-    pkgs-unstable = import nixpkgs-unstable {
-      inherit system;
-      config = { allowUnfree = true; };
-    };
-
-    pkgs-stable = import nixpkgs-stable {
-      inherit system;
-      config = { allowUnfree = true; };
-    };
-
     pkgs = import nixpkgs-unstable {
       inherit system;
       config = { allowUnfree = true; };
     };
   in {
     nixosConfigurations = {
-      nixos-desktop = nixpkgs-unstable.lib.nixosSystem {
-        system = system;
-        pkgs = pkgs-unstable;
-        modules = [
-          ./common.nix
-          ./hardware-specific-desktop.nix
-          ./hardware-configuration-desktop.nix
-	        nvf.nixosModules.default
-        ];
-        specialArgs = {
-          inherit pkgs-stable;
-        };
-      };
-
-      nixos-laptop = nixpkgs-unstable.lib.nixosSystem {
-        system = system;
-        pkgs = pkgs-unstable;
-        modules = [
-          ./common.nix
-          ./hardware-specific-laptop.nix
-          ./hardware-configuration-laptop.nix
-	        nvf.nixosModules.default
-        ];
-        specialArgs = {
-          inherit pkgs-stable;
-        };
-      };
-
-      desktop = nixpkgs-unstable.lib.nixosSystem {
+      desktop = nixpkgs.lib.nixosSystem {
         inherit system pkgs;
         modules = [
           ./hosts/desktop
-	        nvf.nixosModules.default
+	  nvf.nixosModules.default
         ];
       };
 
-      laptop = nixpkgs-unstable.lib.nixosSystem {
+      laptop = nixpkgs.lib.nixosSystem {
         inherit system pkgs;
         modules = [
           ./hosts/laptop
-	        nvf.nixosModules.default
+	  nvf.nixosModules.default
         ];
       };
     };
