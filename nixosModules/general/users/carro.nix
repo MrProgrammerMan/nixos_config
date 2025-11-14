6308cbb714b083a3521e9849cafcdf12cfafff4b
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-11-14 12:38:42 +0100

    Flake content update(desktop)

--- nixosModules/general/users/carro.nix
+++ nixosModules/general/users/carro.nix
@@ -3,7 +3,7 @@
     users.users.carro = {
       isNormalUser = true;
       description = "Caroline";
-      extraGroups = [ "networkmanager" "wheel" ];
+      extraGroups = [ "networkmanager" ];
       hashedPassword = "$6$pneBpmZMJV5BqKi/$n.k2HbZPiWPmhKbGq16/aW0EGDbz2f1OzLfdAqThlJIuNp9G3PerJi1qsj5QM5mrTMuF2A.AmUWATWbhNs6OJ.";
       packages = with pkgs; [
         onedrive
