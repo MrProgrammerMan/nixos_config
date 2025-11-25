c3c18970949349cd67b322f307f286ee321da26a
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-11-25 10:53:04 +0100

    Flake content update(desktop)

--- nixosModules/general/users/cepheus/home.nix
+++ nixosModules/general/users/cepheus/home.nix
@@ -5,7 +5,7 @@
 
     wayland.windowManager.hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.system.hyprland;
+      package = inputs.hyprland.packages.${system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       plugins = with inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}; [
       
