7e986aa00a6b8e3d1a122b9d839b74f835968918
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-11-26 15:34:31 +0100

    Flake content update(desktop)

--- users/cepheus.nix
+++ users/cepheus.nix
@@ -13,6 +13,21 @@
       thunderbird
       spotify
       zettlr
+      signal-desktop
+    ];
+  };
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      rust-lang.rust-analyzer
+      ms-vscode.live-server
+      jnoortheen.nix-ide
+      asvetliakov.vscode-neovim
+      vscjava.vscode-java-pack
+      vscjava.vscode-java-debug
+      redhat.java
+      haskell.haskell
+      justusadam.language-haskell
     ];
   };
 }
