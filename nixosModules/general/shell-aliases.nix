d969919c4932e561924bd0f7b818de964f12ccb6
= <==>
2026-01-05 16:06:47 +0100

    Flake content update(laptop)

--- nixosModules/general/shell-aliases.nix
+++ nixosModules/general/shell-aliases.nix
@@ -3,20 +3,20 @@
     environment.shellAliases = {
       nixsw = ''
         cd /etc/nixos &&
-        sudo git add . &&
-        sudo git commit -m "Flake content update($(hostname))" || true &&
-        sudo git push &&
+        git add . &&
+        git commit -m "Flake content update($(hostname))" || true &&
+        git push &&
         sudo nixos-rebuild switch --flake .#$(hostname)
       '';
       nixup = ''
         cd /etc/nixos &&
-        sudo git add . &&
-        sudo git commit -m "Flake content update($(hostname))" || true &&
-        sudo git push &&
-        sudo nix flake update &&
-        sudo git add . &&
-        sudo git commit -m "Flake update($(hostname))" || true &&
-        sudo git push &&
+        git add . &&
+        git commit -m "Flake content update($(hostname))" || true &&
+        git push &&
+        nix flake update &&
+        git add . &&
+        git commit -m "Flake update($(hostname))" || true &&
+        git push &&
         sudo nixos-rebuild switch --flake .#$(hostname)
       '';
     };
