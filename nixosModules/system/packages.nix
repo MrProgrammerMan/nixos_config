2c901e771381c9103010254584f3ed03308e19f2
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-01-05 08:38:04 +0100

    Flake content update(desktop)

--- nixosModules/system/packages.nix
+++ nixosModules/system/packages.nix
@@ -3,7 +3,12 @@
     environment.systemPackages = with pkgs; [
       libreoffice
     ];
-    programs.git.enable = true;
+    programs.git = {
+      enable = true;
+      config = {
+        global = "credential.https://github.com.useHttpPath";
+      };
+    };
     programs.firefox.enable = true;
   };
 }
