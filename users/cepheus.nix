8f0598684eff18a8ac9f9f339a73c9fd29a222b8
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-02-12 16:01:59 +0100

    Flake content update(desktop)

--- users/cepheus.nix
+++ users/cepheus.nix
@@ -30,7 +30,6 @@
   };
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode-fhs;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       rust-lang.rust-analyzer
       ms-vscode.live-server
