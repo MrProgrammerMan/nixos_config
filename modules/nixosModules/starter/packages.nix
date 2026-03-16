69066a0a6b06f54de3637f12f771331f913b9595
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-03-16 08:56:48 +0100

    Flake content update(desktop)

--- modules/nixosModules/starter/packages.nix
+++ modules/nixosModules/starter/packages.nix
@@ -2,6 +2,7 @@
   flake.nixosModules.packages = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
       libreoffice
+      qt6.full
     ];
     programs.git.enable = true;
     programs.ssh.enableAskPassword = false;
