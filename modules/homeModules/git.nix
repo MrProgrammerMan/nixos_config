1dfb56c00d43f0fa0fb0663ef639021adb5126a3
= <=>
2026-03-22 15:17:32 +0100

    Flake content update(laptop)

--- modules/homeModules/git.nix
+++ modules/homeModules/git.nix
@@ -4,22 +4,13 @@
       enable = true;
       signing = {
         format = "ssh";
+        key = "~/.ssh/id_ed25519.pub";
         signByDefault = true;
       };
       settings = {
         user = {
           name = "MrProgrammerMan";
           email = "jonas.baugerud@proton.me";
-          signingKey = "~/.ssh/id_ed25519.pub";
-        };
-        gpg = {
-          format = "ssh";
-        };
-        commit = {
-          gpgSign = true;
-        };
-        tag = {
-          gpgSign = true;
         };
       };
     };
