005a38a359c16f9007447272d14f354742ba6e27
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-03-22 13:59:15 +0100

    Flake content update(desktop)

--- modules/homeModules/git.nix
+++ modules/homeModules/git.nix
@@ -5,11 +5,11 @@
       signing = {
         format = "ssh";
         signByDefault = true;
+        key = "~/.ssh/id_ed25519.pub";
       };
       settings = {
         user = {
           name = "MrProgrammerMan";
-          signingkey = "~/.ssh/id_ed25519.pub";
         };
       };
     };
