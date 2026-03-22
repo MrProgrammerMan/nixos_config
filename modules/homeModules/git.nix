02ef862ca5daa4a8515ab958d72e0159a5acc94a
MrProgrammerMan <jonas.baugerud@gmail.com>
2026-03-22 13:55:40 +0100

    feat(git): add signing

--- modules/homeModules/git.nix
+++ modules/homeModules/git.nix
@@ -2,28 +2,14 @@
   flake.homeModules.git = { pkgs, ... }: {
     programs.git = {
       enable = true;
-      package = pkgs.git.override { withLibsecret = true; };
       signing = {
-        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPTpoz1uH6bA4ctEIuzDsNWlS9NIbIcT3mg5rVcMUvpI jonas.baugerud@gmail.com";
+        format = "ssh";
         signByDefault = true;
       };
       settings = {
-        gpg = {
-          format = "ssh";
-        };
-        credential.helper = "libsecret";
-        "credential \"https://github.com\"" = {
-          useHttpPath = true;
-          username = "MrProgrammerMan";
-        };
-        user.name = "MrProgrammerMan";
-        url = {
-          "https://github.com/" = {
-            insteadOf = [
-              "gh:"
-              "github:"
-            ];
-          };
+        user = {
+          name = "MrProgrammerMan";
+          signingkey = "~/.ssh/id_ed25519.pub";
         };
       };
     };
