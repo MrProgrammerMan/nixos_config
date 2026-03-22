5f2dd251e8d04c68c4c8ec4a34bc562308f84550
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-03-22 16:16:36 +0100

    Flake content update(laptop)

--- modules/homeModules/git.nix
+++ modules/homeModules/git.nix
@@ -1,17 +1,25 @@
 { ... }: {
-  flake.homeModules.git = { pkgs, ... }: {
+  flake.homeModules.git = { ... }: {
     programs.git = {
       enable = true;
-      signing = {
-        format = "ssh";
-        key = "~/.ssh/id_ed25519.pub";
-        signByDefault = true;
-      };
       settings = {
         user = {
           name = "MrProgrammerMan";
           email = "69722297+MrProgrammerMan@users.noreply.github.com";
         };
+        safe = {
+          directory = "/etc/nixos";
+        };
+        gpg = {
+          ssh = {
+            allowedSignersFile = "~/.ssh/allowed_signers";
+          };
+        };
+      };
+      signing = {
+        format = "ssh";
+        key = "~/.ssh/id_ed25519.pub";
+        signByDefault = true;
       };
     };
   };
