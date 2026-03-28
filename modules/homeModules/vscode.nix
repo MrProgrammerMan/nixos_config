01fd38549fbd8b87ff95b564ead16b5ad67b1b54
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-03-28 09:58:59 +0100

    Flake content update(desktop)

--- modules/homeModules/vscode.nix
+++ modules/homeModules/vscode.nix
@@ -3,15 +3,23 @@
     programs.vscode = {
       enable = true;
       package = pkgs.vscode-fhs;
-      profiles.default.extensions = with pkgs.vscode-extensions; [
-        rust-lang.rust-analyzer
-        ritwickdey.liveserver
-        jnoortheen.nix-ide
-        redhat.java
-        anweber.vscode-httpyac
-        theangryepicbanana.language-pascal
-        myriad-dreamin.tinymist
-      ];
+      profiles.default ={
+        extensions = with pkgs.vscode-extensions; [
+          rust-lang.rust-analyzer
+          ritwickdey.liveserver
+          jnoortheen.nix-ide
+          redhat.java
+          anweber.vscode-httpyac
+          theangryepicbanana.language-pascal
+          myriad-dreamin.tinymist
+        ];
+        userSettings = {
+          "rust-analyzer.cargo.features" = "all";
+          "rust-analyzer.procMacro.ignored" = {
+            "leptos_macro" = [ "server" ];
+          };
+        };
+      };
     };
   };
 }
