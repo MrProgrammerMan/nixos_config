519b401243cd6ee5b728a9c5d6f76ea589372902
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-07-26 13:17:57 +0200

    fix!: use stable nixpkgs. NOTE: may require downgrade of certain boot components if unstable was applied previously

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "System configuration for desktop & laptop";
   
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-26.05"; };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
